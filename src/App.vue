7ff5a4ce478127a3562a75522207e1f190471a1b
llx1001 <llx771436886@163.com>
2021-02-24 18:30:54 +0800

    第四次提交tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
+    <main-tab-bar>
 
+    </main-tab-bar>
   </div>
 </template>
 
 <script>
 
+import MainTabBar from "@/components/content/tabbar/MainTabBar";
+
 export default {
   name: 'App',
   components: {
+    MainTabBar
   }
 }
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+// const originalPush = VueRouter.prototype.push
+// VueRouter.prototype.push = function push(location) {
+//   return originalPush.call(this, location).catch(err => err)
+// }
+
+const Home = () => import('views/home/Home')
+const Category = () => import('views/category/Category')
+const Cart = () => import('views/cart/Cart')
+const Profile = () => import('views/profile/Profile')
+
+
+
+//1.安装插件
+Vue.use(VueRouter)
+
+const routes = [
+  //默认首页
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+//2.创建路由对象实例
+const router = new VueRouter({
+  routes,
+  mode:'history'
+})
+
+
+//3.导出router
+export default router
